e3738a1cf4cbdd11b810d8d6ad78718b9e731b0a
felipe <felipebaise@gmail.com>
2024-11-12 21:31:52 -0300

    Loja Ponto Quente

---
+++ Lojas.py
@@ -0,0 +1,9 @@
+cliente=("Erick")
+mes=("Maio")
+valor=435.00
+desconto=43
+cupom=(cliente+"É43")
+
+
+print("Olá " + cliente + ", em " + mes + " você realizou uma compra no valor de R$" , float(valor) , " e ganhou um desconto de " , int(desconto) , "% em sua próxima compra. Use o cupom " + cupom)
+
---
+++ desafio.py
@@ -0,0 +1,4 @@
+print("Desafio do Caique")
+a=145
+b=234
+print(a+b)
