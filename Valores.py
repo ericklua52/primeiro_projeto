65b55d6ac85bd562601a8c33ae2381677a894774
felipe <felipebaise@gmail.com>
2024-11-13 21:30:19 -0300

    meu primeiro commit

---
+++ Valores.py
@@ -0,0 +1,12 @@
+valor=int(input("insira um valor inteiro: "))
+dobro=valor*2
+triplo=valor*3
+quadrado=valor**2
+raiz_quadrada=(valor**(1/2))
+raiz_cubica=(valor**(1/3))
+
+print(f"o dobro do seu {valor}", {dobro})
+print(f"o triplo do seu {valor}", {triplo}) 
+print(f"o quadrado do seu {valor}", {quadrado}) 
+print(f"a raiz quadrado do seu {valor}", {raiz_quadrada})
+print(f"a raiz cúbica do seu {valor}", {raiz_cubica})
---
+++ operação.py
@@ -0,0 +1,9 @@
+
+nota1=float(input("digite sua nota do 1º bimestre: "))
+nota2=float(input("digite sua nota do 2º bimestre: "))
+nota3=float(input("digite sua nota do 3º bimestre: "))
+nota4=float(input("digite sua nota do 4° bimestre: "))
+soma= nota1+nota2+nota3+nota4
+media =soma/4
+
+print("Olá Caique,sua nota é:",media )
